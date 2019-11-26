c96622580a47a787281f05777efcacf2c9ccef9b
Yevgeniy Skroznikov <yevskro@gmail.com>
2019-11-26 14:38:36 -0500

    finished app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component} from "react";
 import { Route } from  "react-router-dom";
 import "./App.css";
+import Game from '../scenes/Game'
+import Intro from '../scenes/Intro'
 
 const App = () => (
   <div className="App">
-    <Route exact path="/" component={}/>
-    <Route path="/game" component={}>
+    <Route exact path="/" component={Game} />
+    <Route exact path="/game" component={Intro} />
   </div>
 );
 

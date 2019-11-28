906bc4bbe8ea2b6ccdb0cf90e4c339ff52931916
Yevgeniy Skroznikov <yevskro@gmail.com>
2019-11-28 15:54:54 -0500

    fixed merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,11 @@ import Session from './scenes/Session'
 import Intro from './scenes/Intro'
 import { withCookies, useCookies } from 'react-cookie';
 
-<<<<<<< HEAD
-const App = () => (
-  <div className="App">
-    <Route exact path="/" component={Intro} />
-    <Route exact path="/game" component={Game} />
-  </div>
-);
-=======
 const App = (props) => {
   return <div className="App">
           <Route exact path="/" render={(p) => <Intro {...p} cookies={props.cookies}/>} />
           <Route exact path="/session" component={Session} />
         </div>
 };
->>>>>>> intro
 
 export default withCookies(App);

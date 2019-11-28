4331f678e257a714cd8e341176e366f8bd16aea2
Yevgeniy Skroznikov <yevskro@gmail.com>
2019-11-28 16:04:19 -0500

    stubbing out session

--- client/src/scenes/Session/index.js
+++ client/src/scenes/Session/index.js
@@ -1,7 +1,17 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
 
 class Session extends Component {
-    
+    constructor(){
+        super()
+    }
+
+    componentDidMount(){
+        // grab session and update state
+    }
+
+    render(){
+        return <div></div>
+    }
 }
 
 export default Session

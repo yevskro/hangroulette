2d2f09c5c399918f47972ec233a408cb6db8a723
Yevgeniy Skroznikov <yevskro@gmail.com>
2019-12-09 13:25:21 -0500

    added data protection to guess()

--- models/server/ServerGame.js
+++ models/server/ServerGame.js
@@ -1,12 +1,34 @@
-import GameModel from '../Game'
+import GameModel, { GAMESTATUS } from '../Game'
 
 export default class ServerGameModel extends GameModel{
     constructor(mdlGuesses, mdlPlayers, word, gameStatus, hiddenWord){
-        this.validateGuess = (guess) => {
-
-        }
-
         this.guess = (guess) => {
+            /* undefined is to be understood as a malicious data attempt */
+            try{
+                mdlGuesses.validateGuess(guess)
+            }
+            catch(e){
+                return undefined 
+            }
+
+            if(mdlGame.gameStatus !== GAMESTATUS.player){
+            /* client should know not to send a guess if the game is over */
+                return undefined
+            }
+
+            const correct = mdlGuesses.correct()
+            const wrong   = mdlGuesses.wrong()
+            if(correct.includes(guess) || wrong.includes(guess)){
+                /* the client should be responsible for not sending the same guess */
+                return undefined
+            }
+
+            if(hiddenWord.includes(guess)){
+                /* correct guess */
+                
+                /* check if the game is won */
+            }
+            /* if we are here then there was an incorrect guess */
 
         }
 

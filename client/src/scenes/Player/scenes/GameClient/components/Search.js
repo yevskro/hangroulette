dbfae8521c023ea172a7f150127c89f3aa61bcec
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-01-01 18:46:18 -0500

    refactored Search css to follow oocss

--- client/src/scenes/Player/scenes/GameClient/components/Search.js
+++ client/src/scenes/Player/scenes/GameClient/components/Search.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "../../../../../styles/Search.css"
 
 const generateSearch = (status) => {
     if(status){
-        return <div className="search">searching..</div>
+        return <div className="srch srch-lrg srch-styl-1 flx--mdl box--pos-abs-top">searching..</div>
     }
-    return <div className="no-search"></div>
+    return <div className="no-srch"></div>
 }
 
 const Search = (props) => generateSearch(props.status) 

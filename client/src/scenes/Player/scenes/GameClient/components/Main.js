c144585e777d0ad262abae7150a230e1b2ae3420
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-01-01 20:28:04 -0500

    oocss'd main-container

--- client/src/scenes/Player/scenes/GameClient/components/Main.js
+++ client/src/scenes/Player/scenes/GameClient/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
+import "../../../../../styles/Main.css"
 
-const Main = (props) => <div className="main-container">{props.children}</div>
+const Main = (props) => <div className="main-cntnr flx algn-itms--cntr">{props.children}</div>
 
 export default Main

31c2b787ae2c5cbe4c1d1989ae886fcba141daed
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-01-02 00:37:13 -0500

    renamed Common to Utilities to reflect better on its functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from  "react-router-dom";
 import Player from './scenes/Player/Player.js'
 import "../src/styles/Reset.css";
-import "../src/styles/Common.css";
+import "../src/styles/Utilities.css";
 import "../src/styles/App.css";
 
 const App = (props) => {

9cb881b0779d0211302e098d9d265fd12b30f3a3
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-01-02 11:48:08 -0500

    renamed gm-btn to btn

--- client/src/scenes/Player/scenes/GameClient/components/GameButton.js
+++ client/src/scenes/Player/scenes/GameClient/components/GameButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "../../../../../styles/GameButton.css"
 
-const GameButton = (props) => <div className="gm-btn gm-btn clr--wht flx--mdl" onClick={props.onClick}>
-                                            <div className="gm-btn__ptr clr--rd"></div>
+const GameButton = (props) => <div className="btn clr--wht flx--mdl" onClick={props.onClick}>
+                                            <div className="btn__ptr clr--rd"></div>
                                             {props.title}
                                         </div>
 
